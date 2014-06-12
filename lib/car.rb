30cdd969376faf5c0e6ad4a31ca3d27e22f422b8
Jodi Detch <jodidetch@gmail.com>
2014-06-12 19:57:42 -0400

    Added value_depreciated method to car.rb file

--- lib/car.rb
+++ lib/car.rb
@@ -5,19 +5,25 @@
 #has an MSRP (original retail price)
 #has a value (MSRP modified by depreciation, taking the year of manufacture into account)
 #all cars have a constant depreciation of 5% per year
-#has a manager markup
+#has a manager markup (can be set to any value, representing the car lot's profit margin)
 #has a price (value modified by the manager markup)
+require 'date'
 
 class Car
   attr_reader :make, :model, :year_of_manufacture, :original_retail_msrp
   attr_accessor :value_depreciated, :manager_markup, :price
 
-  DEPRECIATION = 0.05
+  DEPRECIATION = -0.05
 
   def initialize (make, model, year_of_manufacture, original_retail_msrp)
     @make = make
     @model = model
     @year_of_manufacture = year_of_manufacture
     @original_retail_msrp = original_retail_msrp
+  end
+
+  def value_depreciated
+    now = Date.today
+    (now.year - year_of_manufacture) * (original_retail_msrp * DEPRECIATION) + original_retail_msrp
   end
 end
