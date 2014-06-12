a5de39f9ae71cd4803470003d3f93f42bdd8e2c8
Jodi Detch <jodidetch@gmail.com>
2014-06-12 17:41:15 -0400

    Created Car class and test_car file

---
+++ lib/car.rb
@@ -0,0 +1,23 @@
+#In this assignment we'll be writing a collection of Ruby classes for a "car lot" application. We'll be able to create a car lot that contains both new and used cars for sale, and used cars may have damages recorded. We will not be writing the application interface itself – no puts or gets anywhere in your code. As the developer you can interact with your classes using binding.pry and test files.
+
+# CAR
+#has a make, model, and year of manufacture
+#has an MSRP (original retail price)
+#has a value (MSRP modified by depreciation, taking the year of manufacture into account)
+#all cars have a constant depreciation of 5% per year
+#has a manager markup
+#has a price (value modified by the manager markup)
+
+class Car
+  attr_reader :make, :model, :year_of_manufacture, :original_retail_msrp
+  attr_accessor :value_depreciated, :manager_markup, :price
+
+  DEPRECIATION = 0.05
+
+  def initialize (make, model, year_of_manufacture, original_retail_msrp)
+    @make = make
+    @model = model
+    @year_of_manufacture = year_of_manufacture
+    @original_retail_msrp = original_retail_msrp
+  end
+end
