6fc546cf0616beb81ebe5f810249d4a524a6bdb5
Jodi Detch <jodidetch@gmail.com>
2014-06-12 17:58:10 -0400

    Added Damaged_car file and required used_car file in it

---
+++ lib/damaged_car.rb
@@ -0,0 +1,7 @@
+#In this assignment we'll be writing a collection of Ruby classes for a "car lot" application. We'll be able to create a car lot that contains both new and used cars for sale, and used cars may have damages recorded. We will not be writing the application interface itself – no puts or gets anywhere in your code. As the developer you can interact with your classes using binding.pry and test files.
+
+# DAMAGE
+#has a description
+#has a cost (how much it would cost to repair)
+
+require_relative 'used_car'
