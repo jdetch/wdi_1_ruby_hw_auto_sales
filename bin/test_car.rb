2ae4bc64ad45067572ec0d088266aa2064c93feb
Jodi Detch <jodidetch@gmail.com>
2014-06-12 20:07:21 -0400

    Added fixed markup, defined price method and successfully tested it

--- bin/test_car.rb
+++ bin/test_car.rb
@@ -1,8 +1,12 @@
 
 require_relative '../lib/car.rb'
 
-my_car = Car.new('Jeep', 'Grand Cherokee', 2014, 12000)
+my_car = Car.new('Jeep', 'Grand Cherokee', 2012, 12000)
 
 puts "My new car is a #{my_car.make} #{my_car.model} from #{my_car.year_of_manufacture} and it cost $#{my_car.original_retail_msrp}."
+puts
+puts "The price of my car has depreciated to $#{my_car.value_depreciated.to_i}."
+puts
+puts "The price of my car after the manager markup was $#{my_car.price.to_i}."
 
 
