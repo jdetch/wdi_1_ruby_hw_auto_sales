15cc65407a533faadf45c20b44f7d13c52a0c143
Jodi Detch <jodidetch@gmail.com>
2014-06-12 20:37:32 -0400

    Successfully added & tested has_damages? method in used_car file

--- bin/test_used_car.rb
+++ bin/test_used_car.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/used_car.rb'
 
-used_car = Used.new(10000)
+used_car = Used.new(10000, true)
+msg = used_car.has_damages? ? "has" : "does not have"
 
 puts "My car now has #{used_car.mileage} miles on it."
+puts
+puts "My car #{msg} damages."
